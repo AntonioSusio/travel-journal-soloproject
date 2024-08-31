eda7664edfd5bb667451c00a606e74bf997e9409
Antonio <antoniosusio@gmail.com>
2024-08-31 13:45:40 +0200

    Added data jsx file with all location props

---
+++ src/data.jsx
@@ -0,0 +1,81 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Oct, 2024",
+        endDate: "24 Oct, 2024",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1570459027562-4a916cc6113f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8TW91bnQlMjBGdWppfGVufDB8fDB8fHww"
+    },
+
+    {
+        title: "Glacier National Park",
+        location: "USA",
+        googleMapsUrl: "https://www.google.com/maps/place/Glacier+National+Park/@48.7552373,-113.8047311,13.04z/data=!4m15!1m8!3m7!1s0x5368901555555555:0xaf16bc2215c55dec!2sGlacier+National+Park!8m2!3d48.7596128!4d-113.7870225!10e1!16zL20vMDFnem5u!3m5!1s0x5368901555555555:0xaf16bc2215c55dec!8m2!3d48.7596128!4d-113.7870225!16zL20vMDFnem5u?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "8 Dec, 2024",
+        endDate: "20 Dec, 2024",
+        description: "Glacier National Park is an American national park located in northwestern Montana, on the Canada–United States border, adjacent to Waterton Lakes National Park in Canada. The two parks are known as the Waterton-Glacier International Peace Park.",
+        imageUrl: "https://images.unsplash.com/photo-1708640577682-d43989da6cb7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        title: "The Seven Sisters",
+        location: "England",
+        googleMapsUrl: "https://www.google.it/maps/place/Seven+Sisters/@50.7477402,0.1898479,15z/data=!4m6!3m5!1s0x47df77587752da9b:0x1f279d6fe0af91b2!8m2!3d50.7477402!4d0.1898479!16zL20vMDFxaDl2!5m1!1e4?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "4 Jan, 2025",
+        endDate: "16 Jan, 2025",
+        description: "The Seven Sisters are a series of chalk cliffs overlooking the English Channel. They are part of the South Downs in East Sussex, between the towns of Seaford and Eastbourne in southern England. They are within the Seven Sisters Country Park.",
+        imageUrl: "https://images.unsplash.com/photo-1686907810912-09b5d75623a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fFRoZSUyMHNldmVuJTIwc2lzdGVyc3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+
+    {
+        title: "Montpellier",
+        location: "France",
+        googleMapsUrl: "https://www.google.it/maps/place/Montpellier,+Francia/@43.6100162,3.8741693,13z/data=!3m1!4b1!4m6!3m5!1s0x12b6af0725dd9db1:0xad8756742894e802!8m2!3d43.6108535!4d3.8761323!16zL20vMGpxMjc!5m1!1e4?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "10 Feb, 2025",
+        endDate: "25 Feb, 2025",
+        description: "The city of Montpellier is located in the hinterland of southern France, 10 km from the Mediterranean coast. The imposing Gothic cathedral of Saint-Pierre de Montpellier, which is characterized by its two conical towers, dates back to 1364. The Antigone district is an exclusive and modern urban development of neoclassical inspiration.",
+        imageUrl: "https://images.unsplash.com/photo-1612263160749-6f79adb37969?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fE1vbnRwZWxsaWVyfGVufDB8fDB8fHww"
+    },
+
+    {
+        title: "Great Wall Of China",
+        location: "China",
+        googleMapsUrl: "https://www.google.it/maps/place/Grande+muraglia+cinese/@40.4319118,116.5678,17z/data=!3m1!4b1!4m6!3m5!1s0x35f121d7687f2ccf:0xd040259b950522df!8m2!3d40.4319077!4d116.5703749!16zL20vMGQyZHEw?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "10 Mar, 2025",
+        endDate: "27 Mar, 2025",
+        description: "The Great Wall, originally known as Wanli changcheng, is a very long series of walls located in present-day China. It was declared a World Heritage Site by UNESCO in 1987 and included in 2007 among the seven wonders of the modern world.",
+        imageUrl: "https://images.unsplash.com/photo-1713346643797-34e3e7188833?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlYXQlMjB3YWxsJTIwb2YlMjBjaGluYXxlbnwwfHwwfHx8MA%3D%3D"
+    },
+
+    {
+        title: "Cinque Terre",
+        location: "Italy",
+        googleMapsUrl: "https://www.google.it/maps/place/Sentiero+Monterosso+-+Vernazza/@44.1363837,9.6762101,14.73z/data=!4m15!1m8!3m7!1s0x12d4ee1b9aabba7b:0xff95cef11a70f4ac!2sCinque+Terre,+SP,+Italia!3b1!8m2!3d44.1460939!4d9.6438793!16zL20vMDM2dms1!3m5!1s0x12d4f1d537a38f75:0xd5d669151fe69a14!8m2!3d44.1383538!4d9.6780998!16s%2Fg%2F11c4b3f5vb?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "17 May, 2025",
+        endDate: "1 Jun, 2025",
+        description: "The Cinque Terre are ancient fishing villages located along the rugged Italian coast. Each of these villages is characterized by colorful houses and vineyards clinging to the steep terraces carved out of the coast.",
+        imageUrl: "https://images.unsplash.com/photo-1697179184049-372b41197a9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fGNpbnF1ZSUyMHRlcnJlfGVufDB8fDB8fHww"
+    },
+
+    {
+        title: "Iguazù Falls",
+        location: "Argentina",
+        googleMapsUrl: "https://www.google.it/maps/place/Iguazu+Falls/@-25.6917862,-54.4420411,16z/data=!4m6!3m5!1s0x94f6ea7339e748e7:0x1c851fea816c962f!8m2!3d-25.690864!4d-54.4408161!16s%2Fg%2F11bw1_75m5?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "10 Jun, 2025",
+        endDate: "25 Jun, 2025",
+        description: "Iguazù Falls are waterfalls generated by the Iguazu River on the border between the Argentine province of Misiones and the Brazilian state of Paraná. They are the largest waterfalls in the world with a width of 7.65 km.",
+        imageUrl: "https://images.unsplash.com/photo-1508710285745-edc8137d6b5b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ5fHxpZ3VhenUlMjBmYWxsc3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+
+    {
+        title: "Christ Redeemer",
+        location: "Brazil",
+        googleMapsUrl: "https://www.google.it/maps/place/Cristo+Redentore/@-22.951916,-43.2104872,15z/data=!4m6!3m5!1s0x997fd5984aa13f:0x9dc984d7019502de!8m2!3d-22.951916!4d-43.2104872!16zL20vMDNneXR3?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D",
+        startDate: "2 Jul, 2025",
+        endDate: "17 Jul, 2025",
+        description: "Christ the Redeemer is a statue in the Art Deco style representing Jesus Christ that is located in the Brazilian city of Rio de Janeiro, located on the top of Mount Corcovado. The statue is 38 meters high, of which 8 are part of the base.",
+        imageUrl: "https://images.unsplash.com/photo-1593995863951-57c27e518295?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hyaXN0JTIwcmVkZWVtZXIlMjBicmF6aWx8ZW58MHx8MHx8fDA%3D"
+    },
+]
