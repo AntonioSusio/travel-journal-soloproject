92dd7d9a50347de0ce3abb3404c52efb7daf797e
Antonio Susio <antoniosusio@gmail.com>
2026-03-12 09:13:40 +0100

    Refactored all the project

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer className="site-footer flex">
+            <p className="footer-text">Antonio Susio ©2026-2027</p>
+        </footer>
+    )
+}
